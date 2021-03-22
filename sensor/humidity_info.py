913a881ffc5d7c82eacf1724da35c679c973c06b
ralph <ralphcastro504@hotmail.com>
2021-03-22 20:57:45 +0100

    module 4 commit V1

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -1,4 +1,7 @@
 from house_info import HouseInfo
+from datetime import date
+from datetime import datetime
+
 
 
 class HumidityData(HouseInfo):
@@ -13,3 +16,6 @@ class HumidityData(HouseInfo):
         recs = super().get_data_by_area("humidity", rec_area)
         return self._convert_data(recs)
 
+    def get_data_by_date(self, rec_date= date.today()):
+        recs = super().get_data_by_date("humidity", rec_date)
+        return self._convert_data(recs)
