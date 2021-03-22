f024a3133705faa1b2d2d73edcc12fbcb0a5c797
ralph <ralphcastro504@hotmail.com>
2021-03-22 20:55:47 +0100

    module 4 commit V1

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -3,8 +3,13 @@ from house_info import HouseInfo
 
 class HumidityData(HouseInfo):
 
-    def _conver_data(self, data):
+    def _convert_data(self, data):
         recs = []
         for rec in data:
             recs.append(float(rec))
         return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("humidity", rec_area)
+        return self._convert_data(recs)
+
